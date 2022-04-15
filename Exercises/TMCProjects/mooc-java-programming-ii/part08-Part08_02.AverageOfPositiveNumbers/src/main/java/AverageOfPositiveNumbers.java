4d0ba512a0198166db3499c1cb34af3ffd638901
hypnoticpenguin <chankevin234@gmail.com>
2022-04-14 21:43:43 -0400

    completed tasks 2

--- Exercises/TMCProjects/mooc-java-programming-ii/part08-Part08_02.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
+++ Exercises/TMCProjects/mooc-java-programming-ii/part08-Part08_02.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
@@ -6,9 +6,23 @@ public class AverageOfPositiveNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int total = 0;
+        int counter = 0;
 
         while (true) {
+            int input = Integer.valueOf(scanner.nextLine());
+            if (input == 0) { // checks for 0 to exit
+                if (total == 0) { //checks total
+                    System.out.println("Cannot calculate the average");
+                    break;
+                }
+                System.out.println((double)total/counter);
+                break;
+            }
 
+            if (input > 0) {
+                counter += 1;
+                total += input;
+            }
         }
     }
 }
